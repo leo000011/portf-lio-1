cf227f8f12b52bbd9da8b7e871ccaee7f2322e46
Leo <leandroano8@gmail.com>
2025-06-21 16:46:33 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import NavBar from './components/NavBar'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from './pages/Home'
+
+const App = () => {
+
+    const isSellerPath = useLocation().pathname.includes("seller")
+  console.log(isSellerPath)
+  return (
+    <div>
+      {isSellerPath ? null : <NavBar />}
+      <div className={`${isSellerPath} ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"`}>
+        <Routes>
+          <Route path='/' element={<Home />} /> 
+        </Routes>
+      </div>
+    </div>
+  )
+}
+
+export default App
